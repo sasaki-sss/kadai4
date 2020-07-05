3a13682e38de718854f9a715fa7184a339288ea8
sasaki.atsuko <a2014as@aiit.ac.jp>
2020-07-05 14:35:26 +0900

    テストコードの修正

--- maxcommon-test.sh
+++ maxcommon-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-result="/tmp/$$result"
-
+# 正常系のテストコード
 ans=15
-./maxcommon.sh 465 360 > ${result}
+./maxcommon.sh 465 360
+result=$?
+
 if [ ${ans} -eq ${result} ] ; then
 	exit 0
 else
 	exit 1
 fi
 
-rm /tmp/$$-*
